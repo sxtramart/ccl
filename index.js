3efdf2534c3e54b3bc8a1850211d00fafd135dbb
sxtramart <54831405+sxtramart@users.noreply.github.com>
2021-09-24 11:26:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ fs.readdir("./commands/", (err, files) => {
 client.on("ready", () => {
     console.log(`${client.user.tag} is ready to play music.`)
     const server = client.voice.connections.size
-    client.user.setActivity({ type: "LOVE", name: `Gấu Hư CấuCấu` })
+    client.user.setActivity({ name: `Gấu Hư CấuCấu` })
 })
 
 client.on("message", async message => {
