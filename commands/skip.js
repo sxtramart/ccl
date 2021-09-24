b7e6a0c07e406b5830766f966673e109b1c50a95
sxtramart <54831405+sxtramart@users.noreply.github.com>
2021-09-24 11:08:06 +0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -4,10 +4,10 @@ module.exports = {
     inVoiceChannel: true,
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
-        if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+        if (!queue) return message.channel.send(`${client.emotes.error} | Không có gì trong hàng đợi ngay bây giờ!`)
         try {
             client.distube.skip(message)
-            message.channel.send(`${client.emotes.success} | Skipped! Now playing:\n${queue.songs[0].name}`)
+            message.channel.send(`${client.emotes.success} | Bỏ qua! Đang phát:\n${queue.songs[0].name}`)
         } catch (e) {
             message.channel.send(`${client.emotes.error} | ${e}`)
         }
