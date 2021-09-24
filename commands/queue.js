d6d6a0cc97803ebfe20d250b6966bfb052ee0e9e
sxtramart <54831405+sxtramart@users.noreply.github.com>
2021-09-24 11:47:20 +0700

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -4,7 +4,7 @@ module.exports = {
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(`${client.emotes.error} | Không có gì để phát!`)
-        const q = queue.songs.map((song, i) => `${i === 0 ? "Playing:" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``).join("\n")
-        message.channel.send(`${client.emotes.queue} | **Server Queue**\n${q}`)
+        const q = queue.songs.map((song, i) => `${i === 0 ? "Đang phát:" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``).join("\n")
+        message.channel.send(`${client.emotes.queue} | **Hàng chờ**\n${q}`)
     }
 }
