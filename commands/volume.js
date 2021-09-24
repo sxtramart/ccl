76168809eaab897136f39436797c56f65f43d429
sxtramart <54831405+sxtramart@users.noreply.github.com>
2021-09-24 11:11:08 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -4,10 +4,10 @@ module.exports = {
     inVoiceChannel: true,
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
-        if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+        if (!queue) return message.channel.send(`${client.emotes.error} | Không có gì trong hàng đợi ngay bây giờ!`)
         const volume = parseInt(args[0])
-        if (isNaN(volume)) return message.channel.send(`${client.emotes.error} | Please enter a valid number!`)
+        if (isNaN(volume)) return message.channel.send(`${client.emotes.error} | Nhập số vô đi má!`)
         client.distube.setVolume(message, volume)
-        message.channel.send(`${client.emotes.success} | Volume set to \`${volume}\``)
+        message.channel.send(`${client.emotes.success} | Âm lượng được đặt thành \`${volume}\``)
     }
 }
