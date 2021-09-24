493cc2a35a09f78d1b17c331d7eff99cff900d57
sxtramart <54831405+sxtramart@users.noreply.github.com>
2021-09-24 11:06:40 +0700

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -4,8 +4,8 @@ module.exports = {
     inVoiceChannel: true,
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
-        if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+        if (!queue) return message.channel.send(`${client.emotes.error} | Không có gì trong hàng đợi ngay bây giờ!`)
         client.distube.resume(message)
-        message.channel.send("Resumed the song for you :)")
+        message.channel.send("Tiếp tục bài hát")
     }
 }
