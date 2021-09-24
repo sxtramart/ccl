753e56722743d91f2c78a27300aa193193461d31
sxtramart <54831405+sxtramart@users.noreply.github.com>
2021-09-24 10:48:17 +0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,5 +1,6 @@
 module.exports = {
     name: "skip",
+    aliases: ["s", "sk"],
     inVoiceChannel: true,
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
